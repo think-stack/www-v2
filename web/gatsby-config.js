689f86b7ee4b104b5d3cb1a047b81a46793c90b2
Asa Smith <smith.asa.la@gmail.com>
2019-10-16 16:46:00 -0400

    update favicon

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/ts-monogram.png`, // This path is relative to the root of the site.
+        icon: `src/images/ts-monogram-green.svg`, // This path is relative to the root of the site.
       },
     },
     {

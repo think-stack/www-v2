3acfdad06ee701dd9b38ca5ec0377e253f8ffd0a
Asa Smith <smith.asa.la@gmail.com>
2019-10-27 14:03:21 -0400

    add gatsby-plugin-styled-components

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-transition-link`,
     `gatsby-transformer-sharp`,
     {

51d9dfc4130a2e7578757614f955d0c7215175da
Vassilis <vas.terzo@gmail.com>
2020-02-01 02:18:53 +0200

    add new campaign template

--- web/src/components/campaignContact.js
+++ web/src/components/campaignContact.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import Img from 'gatsby-image'
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+import { Grid, Box } from "@material-ui/core"
 
-export default function CampaignContact ({ content }) {
+export default function CampaignContact({ content }) {
   return (
-    <GridContainer>
-      <figure>
-        <Img style={{borderRadius: `5rem`}} fluid={content.image.asset.fluid} />
-      </figure>
-      <Detail>
-        <H3>{content.name}</H3>
-        <Link href={`tel:${content.tel}`}>{content.tel}</Link>
-        <Link href={`mailto:${content.email}`}>{content.email}</Link>
-      </Detail>
-    </GridContainer>
+    <Grid container spacing={5}>
+      <Grid item xs={12} lg={6}>
+        <figure>
+          <Img
+            style={{ borderRadius: `50%`, width: 200, height: 200 }}
+            fluid={content.image.asset.fluid}
+          />
+        </figure>
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <Box height="100%" display="flex" alignItems="center">
+          <Box>
+            <H3>{content.name}</H3>
+            <Link href={`tel:${content.tel}`}>{content.tel}</Link>
+            <Link href={`mailto:${content.email}`}>{content.email}</Link>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
-
-const GridContainer = styled.div`
-  align-items: center;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-column-gap: 2rem;
-  grid-row-gap: 2rem;
-  height: 100%;
-`
-
-const Detail = styled.div`
-  text-align: center;
-`
 
 const H3 = styled.h3`
   font-family: var(--headingFont);
@@ -37,5 +34,5 @@ const H3 = styled.h3`
 const Link = styled.a`
   color: var(--black);
   display: block;
-  margin-bottom: .5rem;
+  margin-bottom: 0.5rem;
 `

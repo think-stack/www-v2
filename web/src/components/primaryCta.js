1dd87c76200847540d4a40dd5fbbf4a9c28fdc50
Vassilis <vas.terzo@gmail.com>
2020-03-06 21:06:18 +0200

    fix cta

--- web/src/components/primaryCta.js
+++ web/src/components/primaryCta.js
@@ -1,62 +1,54 @@
-import React from 'react'
+import React from "react"
 import { Link } from "gatsby"
-import styled from 'styled-components'
+import { Container, Box } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 
-export default function Cta ({ content: {heading, body, cta, link}}) {
+const useStyles = makeStyles(theme => ({
+  btn: {
+    border: 0,
+    display: "inline-block",
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    borderRadius: "2rem",
+    padding: "0.75rem 2rem",
+    textTransform: "uppercase",
+    fontFamily: "TTSupermolot-Regular",
+    fontSize: "0.75rem",
+    fontWeight: 900,
+    lineHeight: "1.25rem",
+    letterSpacing: "0.2em",
+    transition: "background-color 600ms ease, color 600ms ease",
+    marginTop: 18,
+    cursor: "pointer",
+  },
+  heading: {
+    color: theme.palette.primary.dark,
+    fontSize: 39,
+    fontWeight: 400,
+    lineHeight: "48px",
+    marginBottom: "0.75rem",
+    textAlign: "center",
+    textTransform: "capitalize",
+  },
+}))
+
+export default function Cta({ content: { heading, body, cta, link } }) {
+  const classes = useStyles()
   // when using this component without a link
   // check if prop has cta text
   const hasLink = cta === undefined ? false : true
 
   return (
-    <StyledContainer>
-      <StyledHeading>{heading}</StyledHeading>
-      <StyledP>{body}</StyledP>
-      {hasLink && <StyledLink to={link}>{cta}</StyledLink> }
-    </StyledContainer>
+    <Container>
+      <Box margin="4.875rem auto 4.5rem" maxWidth={550} textAlign="center">
+        <h2 className={classes.heading}>{heading}</h2>
+        <Box lineHeight={1.5}>{body}</Box>
+        {hasLink && (
+          <Link to={link} className={classes.btn}>
+            {cta}
+          </Link>
+        )}
+      </Box>
+    </Container>
   )
 }
-
-const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 4.875rem auto 4.5rem;
-  max-width: 550px;
-`
-
-const StyledHeading = styled.h2`
-  color: var(--darkGreen);
-  font-size: 39px;
-  font-weight: 400;
-  line-height: 48px;
-  margin-bottom: .75rem;
-  text-align: center;
-  text-transform: capitalize;
-`
-
-const StyledP = styled.p`
-  color: var(--black);
-  font-size: 1rem;
-  line-height: 150%;
-  text-align: center;
-`
-
-const StyledLink = styled(Link)`
-  align-self: center;
-  background-color: var(--green);
-  border-radius: 2rem;
-  color: var(--white);
-  font-family: 'TTSupermolot-Regular';
-  font-size: .75rem;
-  font-weight: 900;
-  letter-spacing: .2rem;
-  line-height: 150%;
-  margin-top: 2rem;
-  max-width: 10.3125rem;
-  min-width: 7.5625rem;
-  padding: .75rem 2rem;
-  text-transform: uppercase;
-
-  &:hover {
-    cursor: pointer;
-  }
-`
